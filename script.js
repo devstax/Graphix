64a89844c89ba60cf6b9efea46b81799689e4fec
staxcodes <stefan.miljkovicc@gmail.com>
2018-11-16 15:04:23 +0100

    ~ Minor Sturctural and Design updates, + Scrolling on click

---
+++ script.js
@@ -0,0 +1,19 @@
+// Scroll to specific values
+// scrollTo is the same
+window.scroll({
+    top: 3500, 
+    left: 0, 
+    behavior: 'smooth' <pre rel="HTML"><code markup="tt" class="language-markup">
+  });
+  
+  // Scroll certain amounts from current position 
+  window.scrollBy({ 
+    top: 200, // could be negative value
+    left: 0, 
+    behavior: 'smooth' 
+  });
+  
+  // Scroll to a certain element
+  document.querySelector('.hello').scrollIntoView({ 
+    behavior: 'smooth' 
+  });
